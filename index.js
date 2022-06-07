57f477733fee482abde9c4d8be0a6c1743239d5d
Carlos Andres Aguinaga Núñez <andres.aguinaga@gmail.com>
2022-06-07 10:35:45 -0500

    update test 2

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const square_array = (arr = []) => {
 
     return arr.map(num => {
 
-        let ret = parseInt(num)
-
-        if (isNaN(ret)) {
-
-            return 'delete me'
-        }
-
-        return ret * ret
+        return parseInt(num) * parseInt(num);
     }).filter(a => !isNaN(a)).sort((a, b) => a - b);
 }
 
 console.log(square_array(['1', 'Carlos', 2, 3, 5, 6, 8, 9]));
 
-//console.log(square_array([-2, -1]));
+console.log(square_array([-2, -1]));
 
-//console.log(square_array([-10, -5, 0, 5, 10]));
+console.log(square_array([-10, -5, 0, 5, 10]));
